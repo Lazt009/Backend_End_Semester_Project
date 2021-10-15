e164e3d058b6869f07fc73a375cefee368a356ac
Lazt009 <shinganems@rknec.edu>
2021-10-15 19:57:38 +0530

    File Deleter Added

--- deafApi/views.py
+++ deafApi/views.py
@@ -13,6 +13,17 @@ from deafApi import models
 
 from deafApi import videoCreater
 
+from django.conf import settings
+
+
+def fileDeleter():
+    print( os.chdir( (settings.MEDIA_ROOT  + '\\Videos') ) )
+    for files in os.listdir( (settings.MEDIA_ROOT  + '\\Videos') ):
+        if files != '.buffer-file':
+            os.remove(files)
+    os.chdir( settings.MEDIA_ROOT )
+    
+
 # Create your views here.
 def default(request):
     # cwd = os.getcwd()
@@ -24,15 +35,23 @@ def default(request):
             D[ videos ] = "path"
     return render(request, "deafApi/DefaultPage.html", {"Data" : D})
 
+    os.chdir()
+
 
 @api_view(['GET'])
 def alphabets(request):
+    # Deleting the files which was present prior to this api call
+    fileDeleter()
+
     alphabet = models.Alphabet.objects.all()
     serializer = Serializers.AlphabetSerializer(alphabet, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
 def words(request):
+    # Deleting the files which was present prior to this api call
+    fileDeleter()
+
     word = models.Word.objects.all()
     serializer = Serializers.WordSerializer(word, many=True)
     return Response(serializer.data)
@@ -40,6 +59,9 @@ def words(request):
 @api_view(['POST'])
 def getVideo(request):
     print(request.data)
+    # Deleting the files which was present prior to this api call
+    fileDeleter()
+
     if "text" in request.data.keys():
         #process text here
         processedText = request.data["text"]
