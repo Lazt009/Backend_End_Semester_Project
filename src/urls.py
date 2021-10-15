20bf151c3abd39f66af5ad66e472b6ac222bfe09
Lazt009 <shinganems@rknec.edu>
2021-10-15 22:50:14 +0530

    Static url added

--- src/urls.py
+++ src/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 # from deafApi.views import index
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('deafApi.urls')),
     path('api-auth/', include('rest_framework.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
